dc51b7ecba71dcbdb9a2977656a17d5a05fdabb5
Andy <andrew.rutherford.6@gmail.com>
2020-07-08 10:32:07 +0700

    table displays dummy log data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
+import LogItem from './LogItem';
 
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  const [logs, setLogs] = useState([
+    {
+      _id: 1,
+      text: 'Log one',
+      priority: 'low',
+      user: 'Frank',
+      created: new Date().toString(),
+    },
+    {
+      _id: 2,
+      text: 'Log two',
+      priority: 'moderate',
+      user: 'Trey',
+      created: new Date().toString(),
+    },
+    {
+      _id: 3,
+      text: 'Log three',
+      priority: 'high',
+      user: 'John',
+      created: new Date().toString(),
+    },
+  ]);
 
-export default App
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log) => (
+            <LogItem key={log._id} log={log} />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
