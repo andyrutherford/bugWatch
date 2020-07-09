4fe72df1e613023228d607b666a2b15f3286fa59
Andy <andrew.rutherford.6@gmail.com>
2020-07-09 10:15:42 +0700

    main now requests logs from db and send them to mainWindow

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import Alert from 'react-bootstrap/Alert';
 import Badge from 'react-bootstrap/Badge';
-
 import LogItem from './LogItem';
 import AddLogItem from './AddLogItem';
+import { ipcRenderer } from 'electron';
 
 const App = () => {
-  const [logs, setLogs] = useState([
-    {
-      _id: 1,
-      text: 'Log one',
-      priority: 'low',
-      user: 'Frank',
-      created: new Date().toString(),
-    },
-    {
-      _id: 2,
-      text: 'Log two',
-      priority: 'moderate',
-      user: 'Trey',
-      created: new Date().toString(),
-    },
-    {
-      _id: 3,
-      text: 'Log three',
-      priority: 'high',
-      user: 'John',
-      created: new Date().toString(),
-    },
-  ]);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    ipcRenderer.send('logs:load');
+
+    ipcRenderer.on('logs:get', (e, logs) => {
+      setLogs(JSON.parse(logs));
+    });
+  }, []);
 
   const [alert, setAlert] = useState({
     show: false,
